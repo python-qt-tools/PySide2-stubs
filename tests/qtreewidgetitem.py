6a4ffa2161ec5fbf3b96227c3cb4fb56cd3f6315
Philippe Fremy <phil.fremy@free.fr>
2022-06-09 23:38:43 +0200

    Fix more types

--- tests/qtreewidgetitem.py
+++ tests/qtreewidgetitem.py
@@ -1,4 +1,6 @@
 from PySide2.QtWidgets import QTreeWidgetItem
+from PySide2.QtCore import Qt
+from PySide2.QtGui import QColor
 
 
 class MyTreeWidgetItem(QTreeWidgetItem):
@@ -13,3 +15,12 @@ b = True    # type: bool
 b = t < t
 b = t == t
 b = t != t
+
+t.setForeground(3, QColor(Qt.red))
+t.setBackground(3, QColor(Qt.red))
+
+t.setData(0, 33, 'bla')
+t.setData(0, Qt.ToolTipRole, 'bla')
+
+t.data(0, 33)
+t.data(0, Qt.ToolTipRole)
