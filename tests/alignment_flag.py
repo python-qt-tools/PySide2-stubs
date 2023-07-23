996f36100a5c7e73905661736a8135e92482b340
Claudio Cilloni <claudio.cilloni@gmail.com>
2023-07-23 15:30:49 +0200

    Accept QtAlignmentFlag in setAligment() methods

---
+++ tests/alignment_flag.py
@@ -0,0 +1,14 @@
+from PySide2.QtCore import Qt
+from PySide2.QtWidgets import QSpinBox, QFormLayout, QGraphicsView, QLineEdit
+
+a = QSpinBox()
+a.setAlignment(Qt.AlignRight)
+
+b = QFormLayout()
+b.setAlignment(Qt.AlignRight)
+
+c = QGraphicsView()
+c.setAlignment(Qt.AlignRight)
+
+d = QLineEdit()
+d.setAlignment(Qt.AlignRight)
