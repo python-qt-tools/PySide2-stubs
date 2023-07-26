64cf838ffeb2a786ee098583bc0394f7476ea1a8
Diogo Rossi <rossi.diogo@gmail.com>
2023-07-26 08:45:30 -0300

    add test for `SIGNAL`

--- tests/signal_slot.py
+++ tests/signal_slot.py
@@ -1,6 +1,6 @@
 import typing
 
-from PySide2.QtCore import Signal, QObject
+from PySide2.QtCore import Signal, QObject, SIGNAL
 
 
 class SomeClassWithSignal(QObject):
@@ -10,6 +10,7 @@ class SomeClassWithSignal(QObject):
 
     def __init__(self) -> None:
         super().__init__()  # note: this is mandatory for mypy to pickup the class attribute access
+        self.connect(self,SIGNAL('clicked()'),self.my_slot_no_arg)
 
     def my_slot_no_arg(self) -> None:
         pass
